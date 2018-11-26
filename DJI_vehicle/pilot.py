c0c658505d381e5bedab02cf99c06d5e782ca804
ajin <zhouyasia@gmail.com>
2018-11-26 23:42:28 +0800

    pilot.py

--- DJI_vehicle/pilot.py
+++ DJI_vehicle/pilot.py
@@ -3,10 +3,32 @@
 
 import socket
 
-port=8000
-host='localhost'
-s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+port = 8000
+host = 'localhost'
+socket_obj = -1
 
-s.sendto(b'Hello World!',(host,port))
+'''
+cmd is down below
 
+"Takeoff",                
+"Landing",
+"MoveUp, 
+"MoveDown, 
+"MoveForward, 
+"MoveBack, 
+"MoveLeft, 
+"MoveRight, 
+"TurnLeft, 
+"TurnRight, 
+'''
+def ExecuteCmd(cmd,x = 0):
+    print(cmd+" " + str(x))
+    global socket_obj
+    if socket_obj == -1:
+        socket_obj = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    socket_obj.sendto(bytes((cmd+" "+ str(x)),encoding = "utf8"),(host,port))
+    return
 
+if __name__ == "__main__":
+
+    ExecuteCmd("MoveUp",2)
